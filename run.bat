914c65b1394f1eb8304e0f2cf4fd2f4c1f4cb617
David Beckwitt <110573014+DVBeckwitt@users.noreply.github.com>
2025-12-03 10:33:37 -0600

    Fix PYTHONPATH setup in run.bat

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ setlocal
 REM Change to the folder where this .bat lives
 cd /d "%~dp0"
 REM Make sure Python can import the package straight from src/
-set "PYTHONPATH=%CD%src;%PYTHONPATH%"
+set "PYTHONPATH=%CD%\src;%PYTHONPATH%"
 REM Run the Python script from the installed package location
 python -m hr_energy_lab.hr_interactive
 REM Keep window open so you can see any messages
